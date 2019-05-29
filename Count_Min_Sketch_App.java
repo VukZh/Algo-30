4c4c71a519b992f3e925cf14a2d83ac817e3b4ab
VukZh <36912856+VukZh@users.noreply.github.com>
2019-05-29 21:48:34 +0600

    Add files via upload

--- Count_Min_Sketch_App.java
+++ Count_Min_Sketch_App.java
@@ -38,7 +38,7 @@ public class Count_Min_Sketch_App {
 
     public static void main(String[] args) {
 
-        String findString = "role"; // слово для поиска
+        String findString = "Stephens"; // слово для поиска
 
         String[] words = parseFile("src\\count_min_sketch_app\\wiki.train.tokens"); // wiki.test.train.tokens - 890 ; wiki.train.tokens - 1 762 978 слов
         System.out.println("total words " + words.length);
@@ -63,13 +63,52 @@ public class Count_Min_Sketch_App {
 
         System.out.println("--------------------"); // другой вариант размера таблицы
 
-        cms1 = new CMSketch(16, 4000);
+        cms1 = new CMSketch(5, 12800);
         for (int i = 0; i < words.length; i++) {
             cms1.add(words[i]);
         }
         System.out.println("approximate result " + cms1.get(findString));
 
 //                cms1.CMTableOut(); // вывод для малых таблиц
-    }
 
+        
+
+
+
+
+        System.out.println("");
+        int MaxSize = 10000; //////////////////////////////////////////////////////////////////////// <
+        System.out.println("- тестирование false positives и false find для неповторяющихся строк -");
+        String [] testN = new String[MaxSize];
+        
+        for (int i = 0; i < MaxSize; i++) { // массив неповторяющихся цифр 1 .. 1 млн
+            testN[i] = String.valueOf(i);
+        }
+        
+        System.out.println("total words " + testN.length);
+        
+        CMSketch cms2 = new CMSketch(4, 16000); ////////////////////////////////////////////////////// <
+        for (int i = 0; i < testN.length; i++) {
+            cms2.add(testN[i]);
+        }
+        System.out.println("тестирование false find");
+        
+        int ff = 0;
+        for (int i = MaxSize+1; i < MaxSize*2+1; i++) { // проверка на положительный результат для незанесенных чисел в Count Min Sketch
+            if (cms2.get(String.valueOf(i)) != 0) {
+                ff ++;
+            }
+        }
+        System.out.println("false find - " + ff + "  percent- " + ((float)ff / MaxSize) * 100);        
+        
+        System.out.println("тестирование false positives");
+        int fp = 0;
+        for (int i = 0; i < MaxSize; i++) { // проверка на результат != 1 для занесенных чисел в Count Min Sketch
+            if (cms2.get(String.valueOf(i)) != 1) {
+                fp ++;
+            }
+        }
+        System.out.println("false positives - " + fp + "  percent- " + ((float)fp / MaxSize) * 100);  
+        
+    }
 }
